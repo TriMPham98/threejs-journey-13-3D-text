182b68122fc5164f82dc6b6215493b240e37bce8
Tri <trimpham98@gmail.com>
2023-06-08 11:35:23 -0700

    Import font loader, instantiate a font loader object, and load the font

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 
 /**
  * Base
@@ -16,8 +17,20 @@ const scene = new THREE.Scene();
 
 /**
  * Textures
- */
+ */ 
 const textureLoader = new THREE.TextureLoader();
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader();
+
+fontLoader.load(
+    "/fonts/helvetiker_regular.typeface.json",
+    (font) => {
+        console.log("Font loaded");
+    }
+);
 
 /**
  * Object
