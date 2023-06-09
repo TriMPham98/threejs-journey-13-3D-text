84aaa4d484f9b36d7621c1a9b4ad54782f9c0761
Tri <trimpham98@gmail.com>
2023-06-08 20:55:33 -0700

    Center the textGeometry with bezel offset for x, y, and z

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ scene.add(axesHelper);
 
 /**
  * Textures
- */ 
+ */
 const textureLoader = new THREE.TextureLoader();
 
 /**
@@ -32,32 +32,30 @@ const textureLoader = new THREE.TextureLoader();
  */
 const fontLoader = new FontLoader();
 
-fontLoader.load(
-    "/fonts/helvetiker_regular.typeface.json",
-    (font) => {
-        const textGeometry = new TextGeometry(
-            "Tri Pham",
-            {
-                font: font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 5, 
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 4
-            }
-        );
-        textGeometry.computeBoundingBox();
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  const textGeometry = new TextGeometry("Tri Pham", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 5,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4,
+  });
+  textGeometry.computeBoundingBox();
+  textGeometry.translate(
+    - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+    - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+  );
 
-
-        const textMaterial = new THREE.MeshBasicMaterial();
-        textMaterial.wireframe = true;
-        const text = new THREE.Mesh(textGeometry, textMaterial);
-        scene.add(text);
-    }
-);
+  const textMaterial = new THREE.MeshBasicMaterial();
+  textMaterial.wireframe = true;
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
+});
 
 /**
  * Sizes
