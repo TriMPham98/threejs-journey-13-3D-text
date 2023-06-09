10136818b77f4069ae13d1b1ab22fbe2ed088d37
Tri <trimpham98@gmail.com>
2023-06-08 22:02:49 -0700

    Remove duplicate material loading

--- src/script.js
+++ src/script.js
@@ -26,7 +26,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("/textures/matcaps/3.png");
+const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
 
 /**
  * Fonts
@@ -58,10 +58,10 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
 //   console.time("donuts");
 
   const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-  const donutMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture});
+  const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture});
 
   for (let i = 0; i < 100; i++) {
-    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+    const donut = new THREE.Mesh(donutGeometry, material);
 
     donut.position.x = (Math.random() - 0.5) * 10;
     donut.position.y = (Math.random() - 0.5) * 10;
@@ -78,9 +78,8 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
 
 //   console.timeEnd("donuts");
 
-  const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
   //   textMaterial.wireframe = true;
-  const text = new THREE.Mesh(textGeometry, textMaterial);
+  const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
 });
 
