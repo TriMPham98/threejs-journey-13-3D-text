0ab3e623412e418e41b6b06064576b8673d12a87
Tri <trimpham98@gmail.com>
2023-06-08 21:27:16 -0700

    Add concise center for textGeometry

--- src/script.js
+++ src/script.js
@@ -44,12 +44,15 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     bevelOffset: 0,
     bevelSegments: 4,
   });
-  textGeometry.computeBoundingBox();
-  textGeometry.translate(
-    - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-    - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-    - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-  );
+
+//   textGeometry.computeBoundingBox();
+//   textGeometry.translate(
+//     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+//     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+//     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+//   );
+
+textGeometry.center();
 
   const textMaterial = new THREE.MeshBasicMaterial();
   textMaterial.wireframe = true;
